fd891854616fc95dea3421954078c2e99ddb92dd
Rafał Harabień <rafalh92@outlook.com>
2018-12-05 21:10:59 +0100

    Add more format_volume tests and simlify code

--- tests/format.rs
+++ tests/format.rs
@@ -7,8 +7,8 @@ use std::io::prelude::*;
 
 use fscommon::BufStream;
 
-const KB: u32 = 1024;
-const MB: u32 = KB * 1024;
+const KB: u64 = 1024;
+const MB: u64 = KB * 1024;
 const TEST_STR: &str = "Hi there Rust programmer!\n";
 
 type FileSystem = fatfs::FileSystem<BufStream<io::Cursor<Vec<u8>>>>;
@@ -18,12 +18,34 @@ fn basic_fs_test(fs: &FileSystem) {
     let entries = root_dir.iter().map(|r| r.unwrap()).collect::<Vec<_>>();
     assert_eq!(entries.len(), 0);
 
-    let mut file = root_dir.create_file("short.txt").expect("create file");
-    file.truncate().expect("truncate file");
-    file.write_all(&TEST_STR.as_bytes()).expect("write file");
+    let subdir1 = root_dir.create_dir("subdir1").expect("create_dir subdir1");
+    let subdir2 = root_dir.create_dir("subdir1/subdir2 with long name").expect("create_dir subdir2");
 
-    let entries = root_dir.iter().map(|r| r.unwrap()).collect::<Vec<_>>();
-    assert_eq!(entries.len(), 1);
+    let test_str = TEST_STR.repeat(1000);
+    {
+        let mut file = subdir2.create_file("test file name.txt").expect("create file");
+        file.truncate().expect("truncate file");
+        file.write_all(test_str.as_bytes()).expect("write file");
+    }
+
+    let mut file = root_dir.open_file("subdir1/subdir2 with long name/test file name.txt").unwrap();
+    let mut content = String::new();
+    file.read_to_string(&mut content).expect("read_to_string");
+    assert_eq!(content, test_str);
+
+    let filenames = root_dir.iter().map(|r| r.unwrap().file_name()).collect::<Vec<String>>();
+    assert_eq!(filenames, ["subdir1"]);
+
+    let filenames = subdir2.iter().map(|r| r.unwrap().file_name()).collect::<Vec<String>>();
+    assert_eq!(filenames, [".", "..", "test file name.txt"]);
+
+    subdir1.rename("subdir2 with long name/test file name.txt", &root_dir, "new-name.txt").expect("rename");
+
+    let filenames = subdir2.iter().map(|r| r.unwrap().file_name()).collect::<Vec<String>>();
+    assert_eq!(filenames, [".", ".."]);
+
+    let filenames = root_dir.iter().map(|r| r.unwrap().file_name()).collect::<Vec<String>>();
+    assert_eq!(filenames, ["subdir1", "new-name.txt"]);
 }
 
 fn test_format_fs(opts: fatfs::FormatOptions, total_bytes: u64) -> FileSystem {
@@ -40,7 +62,7 @@ fn test_format_fs(opts: fatfs::FormatOptions, total_bytes: u64) -> FileSystem {
 
 #[test]
 fn test_format_1mb() {
-    let total_bytes = 1 * MB as u64;
+    let total_bytes = 1 * MB;
     let mut opts: fatfs::FormatOptions = Default::default();
     opts.total_sectors = (total_bytes / 512) as u32;
     let fs = test_format_fs(opts, total_bytes);
@@ -49,7 +71,7 @@ fn test_format_1mb() {
 
 #[test]
 fn test_format_8mb() {
-    let total_bytes = 8 * MB as u64;
+    let total_bytes = 8 * MB;
     let mut opts: fatfs::FormatOptions = Default::default();
     opts.total_sectors = (total_bytes / 512) as u32;
     let fs = test_format_fs(opts, total_bytes);
@@ -58,7 +80,7 @@ fn test_format_8mb() {
 
 #[test]
 fn test_format_50mb() {
-    let total_bytes = 50 * MB as u64;
+    let total_bytes = 50 * MB;
     let mut opts: fatfs::FormatOptions = Default::default();
     opts.total_sectors = (total_bytes / 512) as u32;
     let fs = test_format_fs(opts, total_bytes);
@@ -68,9 +90,24 @@ fn test_format_50mb() {
 
 #[test]
 fn test_format_512mb() {
-    let total_bytes = 2 * 1024 * MB as u64;
+    let total_bytes = 2 * 1024 * MB;
     let mut opts: fatfs::FormatOptions = Default::default();
     opts.total_sectors = (total_bytes / 512) as u32;
+    let fs = test_format_fs(opts, total_bytes);
+    assert_eq!(fs.fat_type(), fatfs::FatType::Fat32);
+}
+
+fn create_format_options(total_bytes: u64, bytes_per_sector: Option<u16>) -> fatfs::FormatOptions {
+    let mut opts: fatfs::FormatOptions = Default::default();
+    opts.total_sectors = (total_bytes / bytes_per_sector.unwrap_or(512) as u64) as u32;
+    opts.bytes_per_sector = bytes_per_sector;
+    opts
+}
+
+#[test]
+fn test_format_512mb_4096sec() {
+    let total_bytes = 1 * 1024 * MB;
+    let opts = create_format_options(total_bytes, Some(1024));
     let fs = test_format_fs(opts, total_bytes);
     assert_eq!(fs.fat_type(), fatfs::FatType::Fat32);
 }
