e66ebfa82317e4076830f13444a64d6182293155
Cole Helbling <cole.e.helbling@outlook.com>
2026-02-02 12:36:32 -0800

    Add a failing test for FAT32 images
    fsck.vfat doesn't error when in `-n` mode, so we pass `-y` to have it
    fix anything it finds, and if it fixes something it will exit with status
    code 1.

--- tests/fsck.rs
+++ tests/fsck.rs
@@ -46,3 +46,57 @@ fn test_fsck_1mb() {
         .expect("wait on fsck");
     assert!(fsck_status.success(), "fsck was not successful ({fsck_status:?})");
 }
+
+#[test]
+fn test_fsck_33mb_fat32() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    let disk_img = "/tmp/test-fat32.img";
+
+    let mut image = std::fs::OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open(disk_img)
+        .expect("open temporary image file");
+    let len = 33 * MB;
+    image.set_len(len as u64).expect("set_len on temp file");
+
+    fatfs::format_volume(
+        &mut fatfs::StdIoWrapper::from(image.try_clone().expect("clone tempfile")),
+        fatfs::FormatVolumeOptions::new().fat_type(fatfs::FatType::Fat32),
+    )
+    .expect("format volume");
+
+    let fsck_status = std::process::Command::new("fsck.vfat")
+        .args(&["-y", disk_img])
+        .spawn()
+        .expect("spawn fsck")
+        .wait()
+        .expect("wait on fsck");
+    assert!(fsck_status.success(), "fsck was not successful ({fsck_status:?})");
+
+    let fs = fatfs::FileSystem::new(image, fatfs::FsOptions::new()).expect("open fs");
+    fs.root_dir().create_dir("dir1").expect("create dir1");
+    fs.root_dir()
+        .create_file("root file.bin")
+        .expect("create root file")
+        .write_all(&[0xab; (16 * KB) as usize])
+        .expect("root file write");
+    let dir2 = fs.root_dir().create_dir("dir2").expect("create dir2");
+    dir2.create_dir("subdir").expect("subdir");
+    dir2.create_file("file1")
+        .expect("file1")
+        .write_all(b"testing 1 2 1 2")
+        .expect("file 1 write");
+    core::mem::drop(dir2);
+    core::mem::drop(fs);
+
+    let fsck_status = std::process::Command::new("fsck.vfat")
+        .args(&["-y", disk_img])
+        .spawn()
+        .expect("spawn fsck")
+        .wait()
+        .expect("wait on fsck");
+    assert!(fsck_status.success(), "fsck was not successful ({fsck_status:?})");
+}
