2f1bf3e0f62e6b70d2a3935830bcad5383fa18a0
Cole Helbling <cole.e.helbling@outlook.com>
2026-03-01 15:12:57 -0800

    Update FAT32 FSInfo after root dir allocation (#108)
    * Add a failing test for FAT32 images
    
    fsck.vfat doesn't error when in `-n` mode, so we pass `-y` to have it
    fix anything it finds, and if it fixes something it will exit with status
    code 1.
    
    * fixup: actually test that fsck.vfat doesn't repair anything in test_fsck_1mb
    
    * Use a common function for fscking Fat images
    
    * Update FAT32 FSInfo after root dir allocation
    
    Overwrite the FSInfo sector once the root directory cluster is allocated
    so free-cluster counts match the FAT and fresh images pass fsck cleanly.
    
    Prior to this change, FAT32 disks created by this library would fail `fsck.vfat` like so:
    
        fsck.fat 4.2 (2021-01-31)
        Free cluster summary uninitialized (should be 66535)
          Auto-setting.
    
        *** Filesystem was changed ***
        Writing changes.
        /tmp/test-fat32.img: 0 files, 1/66536 clusters
    
    Now, they pass:
    
        fsck.fat 4.2 (2021-01-31)
        /tmp/test-fat32.img: 0 files, 1/66536 clusters
    
    * Add test for FAT16 as well

--- tests/fsck.rs
+++ tests/fsck.rs
@@ -1,26 +1,31 @@
 #![cfg(target_os = "linux")]
-use fatfs::Write;
+
+use fatfs::{FatType, Write};
 
 const KB: u32 = 1024;
 const MB: u32 = KB * 1024;
 
-#[test]
-fn test_fsck_1mb() {
-    let _ = env_logger::builder().is_test(true).try_init();
+fn create_and_fsck_image(fat_type: FatType, size: u32) {
+    let mut disk_img =
+        tempfile::NamedTempFile::with_suffix(format!("-{fat_type:?}-{size}.img")).expect("create named tempfile");
+    let disk_img_path = disk_img.path().display().to_string();
 
-    let mut image = std::fs::OpenOptions::new()
-        .write(true)
-        .read(true)
-        .create(true)
-        .open("/tmp/test.img")
-        .expect("open temporary image file");
-    image.set_len(MB as u64).expect("set_len on temp file");
+    let image = disk_img.as_file_mut();
+    image.set_len(size as u64).expect("set_len on temp file");
 
     fatfs::format_volume(
         &mut fatfs::StdIoWrapper::from(image.try_clone().expect("clone tempfile")),
-        fatfs::FormatVolumeOptions::new().total_sectors(MB / 512),
+        fatfs::FormatVolumeOptions::new()
+            .fat_type(fat_type)
+            .total_sectors(size / 512),
     )
     .expect("format volume");
+
+    let fsck_status = std::process::Command::new("fsck.vfat")
+        .args(&["-y", &disk_img_path])
+        .status()
+        .expect("get fsck.vfat status");
+    assert!(fsck_status.success(), "fsck was not successful ({fsck_status:?})");
 
     let fs = fatfs::FileSystem::new(image, fatfs::FsOptions::new()).expect("open fs");
     fs.root_dir().create_dir("dir1").expect("create dir1");
@@ -39,10 +44,29 @@ fn test_fsck_1mb() {
     core::mem::drop(fs);
 
     let fsck_status = std::process::Command::new("fsck.vfat")
-        .args(&["-n", "/tmp/test.img"])
-        .spawn()
-        .expect("spawn fsck")
-        .wait()
-        .expect("wait on fsck");
+        .args(&["-y", &disk_img_path])
+        .status()
+        .expect("get fsck.vfat status");
     assert!(fsck_status.success(), "fsck was not successful ({fsck_status:?})");
+}
+
+#[test]
+fn test_fsck_1mb_fat12() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    create_and_fsck_image(FatType::Fat12, MB);
+}
+
+#[test]
+fn test_fsck_33mb_fat16() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    create_and_fsck_image(FatType::Fat16, 33 * MB);
+}
+
+#[test]
+fn test_fsck_33mb_fat32() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    create_and_fsck_image(FatType::Fat32, 33 * MB);
 }
